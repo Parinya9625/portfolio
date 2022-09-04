050aacdd71d2030027fe88ab28fc1831205356d2
Parinya9625 <parinya9625@hotmail.com>
2022-09-04 22:46:53 +0700

    add canvas context

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,6 @@
 import { writable } from "svelte/store";
+import type { Writable } from "svelte/store";
+import type { Camera } from "three";
+import type { ThrelteContext } from "@threlte/core";
 
-export const isModelLoading = writable(true);
+export const useCanvas: Writable<ThrelteContext> = writable();
