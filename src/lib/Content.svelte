5507ef7e42855c71674b8d47cd25e1b5a7dfa057
Parinya9625 <parinya9625@hotmail.com>
2022-09-07 13:36:33 +0700

    small hover fix

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -183,4 +183,10 @@
         justify-content: center;
         align-items: center;
     }
+    
+    .top-btn:hover {
+        background-color: rgba(255, 255, 255, 1);
+        box-shadow: 0 0 5px 0 rgba(0,0,0,0.1);
+        transform: scale(1.05, 1.05);
+    }
 </style>
