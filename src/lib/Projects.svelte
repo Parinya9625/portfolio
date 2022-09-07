c6ef4faa89b38b4f3688b52d9ad3b2bde06a7bee
Parinya9625 <parinya9625@hotmail.com>
2022-09-07 21:38:13 +0700

    Add projects page

---
+++ src/lib/Projects.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import ProjectsSvg from "../assets/projects.svg";
+</script>
+
+<section>
+    <div class="header">
+        <img src={ProjectsSvg} alt="projects" />
+    </div>
+</section>
+
+<style>
+    section {
+        grid-template-rows: min-content auto;
+    }
+    .header {
+        grid-column: 2 / 12;
+        display: flex;
+        align-items: center;
+        margin-top: 10vmin;
+    }
+    .header > img {
+        height: 10vmin;
+    }
+</style>
+
