5082b9cef88c572487abb41727176b8937edd1d1
Parinya9625 <parinya9625@hotmail.com>
2022-09-07 21:37:00 +0700

    Add about me page

---
+++ src/lib/AboutMe.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import AbouteMeSvg from "../assets/about_me.svg";
+    import SkillsSvg from "../assets/skills.svg";
+</script>
+
+<section>
+    <div>
+        <img src={AbouteMeSvg} alt="about me detail" />
+
+        <img src={SkillsSvg} alt="parinya skills" />
+    </div>
+</section>
+
+<style>
+    section > div {
+        grid-column: 8 / 12;
+        display: grid;
+        align-items: center;
+    }
+
+
+    @media only screen and (max-width: 1024px) {
+        section > div {
+            grid-column: 2 / 3;
+            gap: 5vmin;
+            height: fit-content;
+        }
+    }
+
+</style>
